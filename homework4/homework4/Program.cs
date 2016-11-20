74d57ddd5898e87b8866ac813ad520f9e3b19947
Vitalii Rurenko <vitaliiru@nice.com>
2016-11-20 19:08:39 +0200

    change

--- homework4/homework4/Program.cs
+++ homework4/homework4/Program.cs
@@ -11,31 +11,30 @@ namespace homework4
         public static void SWAP(int a, int b)
         {
             int tmp = 0;
-                   
-               tmp = a;
-               a = b;
-               b = tmp;
-               Console.WriteLine("A= "+a + "  replaced value of A");
-               Console.WriteLine("B= "+b + "  replaced value of B");
-           }
+            tmp = a;
+            a = b;
+            b = tmp;
+          }
+
+
         static void Main(string[] args)
         {
-            
-            int size = 5;  
-            int[] array1 = new int[]  {5, 4, 1, 3, 9 };
-         //   int[] array2 = new int[5];
-            Console.WriteLine("[{0}]", string.Join(", ", array1));
-            for (int i=1; i<size; i++ )
+    
+            int[] array1 = new int[]  {5, 4, 1, 3, 9};
+             Console.WriteLine("[{0}]", string.Join(", ", array1));
+         
+            for (int i=1; i<array1.Length; i++ )
             {
-                for (int j = 1; j < size; j++)
+                for (int j = 1; j < array1.Length; j++)
                 {
                     if (array1[j] < array1[j-1])
                     {
                         Console.WriteLine(array1[j]);
                         Console.WriteLine(array1[j-1]);
-                        SWAP(array1[j], array1[j-1]);
+                        SWAP(array1[j], array1[j - 1]);
                        
-                      Console.WriteLine("[{0}]", string.Join(", ", array1));
+
+                        Console.WriteLine("[{0}]", string.Join(", ", array1));
                         Console.WriteLine(array1[j]);
                         Console.WriteLine(array1[j - 1]);
 
