112fcb8910c0a68d7e2fe776dac7134444697a81
Vitalii Rurenko <rurenkov@gmail.com>
2016-11-20 21:23:10 +0200

    Buble sort

--- homework4/homework4/Program.cs
+++ homework4/homework4/Program.cs
@@ -8,39 +8,41 @@ namespace homework4
 {
     class Program
     {
+        
+        // swap method;
+        /*
         public static void SWAP(int a, int b)
         {
             int tmp = 0;
             tmp = a;
             a = b;
             b = tmp;
-          }
 
+        }
 
+    */
         static void Main(string[] args)
         {
     
             int[] array1 = new int[]  {5, 4, 1, 3, 9};
              Console.WriteLine("[{0}]", string.Join(", ", array1));
-         
+            int tmp = 0;         
             for (int i=1; i<array1.Length; i++ )
             {
                 for (int j = 1; j < array1.Length; j++)
                 {
                     if (array1[j] < array1[j-1])
                     {
-                        Console.WriteLine(array1[j]);
-                        Console.WriteLine(array1[j-1]);
-                        SWAP(array1[j], array1[j - 1]);
-                       
 
-                        Console.WriteLine("[{0}]", string.Join(", ", array1));
-                        Console.WriteLine(array1[j]);
-                        Console.WriteLine(array1[j - 1]);
+                        tmp = array1[j];
+                        array1[j] = array1[j - 1];
+                        array1[j - 1] = tmp;
+                      }
+                    Console.WriteLine(array1[j]);
+                    Console.WriteLine(array1[j - 1]);
 
-                    }
                 }
-               
+
             }
             Console.WriteLine("[{0}]", string.Join(", ", array1));
             
