09a9eba8894de794fa68c33c316a63840e8688e3
Vitalii Rurenko <vitaliiru@nice.com>
2016-11-21 17:41:41 +0200

    insert sorting

--- homework4/homework4/Program.cs
+++ homework4/homework4/Program.cs
@@ -24,6 +24,7 @@ namespace homework4
     
             int[] array1 = new int[]  {5, 4, 1, 9, 1};
             Console.WriteLine("[{0}]", string.Join(", ", array1));
+
             for (int i=1; i<array1.Length; i++ )
             {
                 for (int j = 1; j < array1.Length; j++)
@@ -39,8 +40,6 @@ namespace homework4
             Console.WriteLine("************************");
             Console.WriteLine("******insert************");
 
-
-
             int[] array2 = new int[] { 9, 6, 3, 1, 4 };
             Console.WriteLine("[{0}]", string.Join(", ", array2));
             for (int i = 1; i < array2.Length; i++)
@@ -52,14 +51,43 @@ namespace homework4
                         SWAP(ref array2[j - 1], ref array2[j]);
                         
                         j--;
-                 
                     }
                 }
 
             }
 
             Console.WriteLine("[{0}]", string.Join(", ", array2));
+            Console.WriteLine("************************");
+            Console.WriteLine("*******stack************");
 
+            /*
+             Push – push data on stack
+             Pop – pop data from stack
+             IsEmpty – is there any data in stack?
+             IsFull – is there stack full?
+             Peek – get data from stack without pop
+              */
+
+            int size = 0;
+            Console.WriteLine("Enter the size of an array");
+            while ((!int.TryParse(Console.ReadLine(), out size)) || size == 0)
+            {
+                Console.WriteLine("this is BUKWA or 0");
+                Console.WriteLine("Try again..");
+            }
+            Console.WriteLine("you selected: " + size + " type each element and press enter.");
+
+            int[] array3 = new int[size];
+            for (int j = 0; j < size; j++)
+            {
+                // array1[j] = Convert.ToInt32(Console.ReadLine());
+                while ((!Int32.TryParse(Console.ReadLine(), out array3[j])) || array3[j] < 0)
+                {
+                    Console.WriteLine("Try again..");
+                }
+            }
+            //verify what we have in array
+            Console.WriteLine("[{0}]", string.Join(", ", array3));
 
 
 
